8d4a839d01de4e641a3d7d846b0179dd511825b5
amartinsmg <andremindurimg@gmail.com>
2021-12-11 10:29:19 -0300

    denom/numer%0

--- Js solo.js
+++ Js solo.js
@@ -1,20 +1,32 @@
 function quadraticFunction(a, b, c) {
     if ((!isNaN(a)) && (!isNaN(b)) && (!isNaN(c)) && (a != 0)) {
         var delta, deltaRoot, x1, x2, aa = 2 * a;
-        delta = Math.pow(b, 2) - 4 * a * c;
+        delta = b ** 2 - 4 * a * c;
         if (delta < 0) {
             return ("This quadratic function don't have any real value.")
         } else if (delta == 0) {
-            x1 = - b / aa;
-            return (`x = ${x1}`);
+            if (-b % aa == 0) {
+                return (`x = ${- b / aa}`);
+            } else {
+                return (`x = ${-b}/${aa}`);
+            }
         } else {
             deltaRoot = Math.sqrt(delta);
             if (Number.isInteger(deltaRoot) === true) {
-                x1 = ((-b) + deltaRoot) / aa;
-                x2 = ((-b) - deltaRoot) / aa;
+                let numer1 = (-b) + deltaRoot, numer2 = (-b) - deltaRoot
+                if (numer1 % aa == 0) {
+                    x1 = numer1 / aa;
+                } else {
+                    x2 = `${numer2}/ ${aa}`;
+                }
+                if (numer2 % aa == 0) {
+                    x2 = numer2 / aa;
+                } else {
+                    x2 = `${numer2}/ ${aa}`;
+                }
             } else if (b == 0) {
-                x1 = `(-√${delta})/${aa}`;
-                x2 = `(√${delta})/${aa}`;
+                x1 = `(-√${delta})/ ${aa}`;
+                x2 = `(√${delta})/ ${aa}`;
             } else {
                 x1 = `(${-b} -√${delta})/ ${aa}`;
                 x2 = `(${-b} +√${delta})/ ${aa}`;
@@ -27,4 +39,4 @@ function quadraticFunction(a, b, c) {
 };
 
 
-//console.log(quadraticFunction(-1, 0, 5)); /* to test */
+//console.log(quadraticFunction(4, -4, 1)); /* to test */
