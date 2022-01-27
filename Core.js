a4cef0c65e0e47388e8fa72e415bdbe42ce820f5
amartinsmg <andremindurimg@gmail.com>
2022-01-27 20:30:47 -0300

    PartingScript

--- Core.js
+++ Core.js
@@ -1,9 +1,3 @@
-const AINPUT = document.querySelector("#aValue"),
-    BINPUT = document.querySelector("#bValue"),
-    CINPUT = document.querySelector("#cValue"),
-    FORM = document.querySelector("form");
-
-
 //Function that prints initial quadratic function
 
 function formQF(a, b, c) {
@@ -256,35 +250,6 @@ function quadraticFunction(a, b, c) {
 }
 
 
-//Function that calls the others
-
-function calculate() {
-    const A = parseInt(AINPUT.value),
-        B = parseInt(BINPUT.value),
-        C = parseInt(CINPUT.value),
-        YZERO = document.querySelector("#y-zero"),
-        RESULTDIV = document.querySelector("#result-yzero"),
-        VERTEX = document.querySelector("#coordinates"),
-        RESULTCOORDINATES = document.querySelector("#result-coordinates"),
-        FORM = document.querySelector(".function-form");
-
-    try {
-        const [Y0, COORDINATES] = quadraticFunction(A, B, C);
-        FORM.innerHTML = formQF(A, B, C);
-        YZERO.innerHTML = "When y = 0:";
-        RESULTDIV.innerHTML = Y0;
-        VERTEX.innerHTML = "Vertex Coordinates:";
-        RESULTCOORDINATES.innerHTML = COORDINATES;
-    } catch (e) {
-        FORM.innerHTML = "y = ax\u00B2 + bx + c";
-        YZERO.innerHTML = "This is NOT a Quadratic Function";
-        RESULTDIV.innerHTML = e;
-        VERTEX.innerHTML = null;
-        RESULTCOORDINATES.innerHTML = null;
-    }
-}
-
-
 //Test function
 
 /* void function (a, b, c) {
@@ -294,33 +259,3 @@ function calculate() {
         console.warn(e)
     }
 }(-2, -8, 16); //Ok */
-
-
-//Functions that change the document
-
-FORM.onsubmit = () => {
-    calculate();
-    return false;
-}
-
-AINPUT.onkeydown = (e) => {
-    if (e.keyCode == 13) {
-        BINPUT.focus();
-        BINPUT.value = "";
-        return false;
-    }
-}
-
-BINPUT.onkeydown = (e) => {
-    if (e.keyCode == 13) {
-        CINPUT.focus();
-        CINPUT.value = "";
-        return false;
-    }
-}
-
-CINPUT.onkeydown = (e) => {
-    if (e.keyCode == 13) {
-        return true;
-    }
-}
