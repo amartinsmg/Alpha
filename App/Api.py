d1c1f0147357b91e53e772c7129a5e500110e070
amartinsmg <andremindurimg@gmail.com>
2022-03-19 09:14:01 -0300

    Adding flask

--- App/Api.py
+++ App/Api.py
@@ -1,22 +1,30 @@
+from flask import Flask, request, send_file
 import numpy as np
 from matplotlib import pyplot as plt
+from distutils.debug import DEBUG
 
-a = int(input())
-b = int(input())
-c = int(input())
-delta = b ** 2 - 4 *a * c
-if delta > 0:
-    x1 = (-b - np.sqrt(delta))/(2 * a)
-    x2 = (-b + np.sqrt(delta))/(2 * a)
-else:
-    x1 = -b / (2 * a)
-    x2 = x1
+app = Flask(__name__)
+app.config["DEBUG"] = True
 
-x = np.arange((x1 - 2), (x2 + 2), .1)
-y = a * x ** 2 + b * x + c
+@app.route('/plot-graphic', methods=['GET'])
+def plot_graphic():
+    a = request.args.get('a')
+    b = request.args.get('b')
+    c = request.args.get('c')
+    delta = b ** 2 - 4 *a * c
+    if delta > 0:
+        x1 = (-b - np.sqrt(delta))/(2 * a)
+        x2 = (-b + np.sqrt(delta))/(2 * a)
+    else:
+        x1 = -b / (2 * a)
+        x2 = x1
 
-fig = plt.figure()
-plt.plot(x, y)
-#plt.show()
+    x = np.arange((x1 - 2), (x2 + 2), .1)
+    y = a * x ** 2 + b * x + c
 
-fig.savefig("plot.png")
+    fig = plt.figure()
+    plt.plot(x, y)
+    #plt.show()
+    fig.savefig("plot.svg")
+
+    return send_file("plot.svg", mimetype='image/svg')
