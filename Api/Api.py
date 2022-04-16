e5582b5c2c0ec945166ee154634144dceabd6692
amartinsmg <andremindurimg@gmail.com>
2022-04-16 14:56:13 -0300

    Update to accept fractional and floating values

--- Api/Api.py
+++ Api/Api.py
@@ -3,8 +3,7 @@ from io import StringIO
 from math import sqrt
 from matplotlib import pyplot as plt
 from numpy import arange
-from sympy import Rational as rational, solve, symbols
-
+from sympy import Rational as rational, solve, symbols, sympify
 
 app = Flask(__name__)
 
@@ -14,12 +13,14 @@ def plot_graphic(a, b, c, delta):
         xMin = (-b - sqrt(delta)) / (2 * a)
         xMax = (-b + sqrt(delta)) / (2 * a)
         if a < 0:
-            xMin, xMax = xMin, xMax
+            xMin, xMax = xMax, xMin
     else:
         xMin = xMax = -b / (2 * a)
-    xMin -= 2
-    xMax += 2
-    x = arange(xMin, xMax, .01)
+    extraValue = max(2, abs(xMax - xMin)/5)
+    xMin -= extraValue
+    xMax += extraValue
+    step = (xMax - xMin) / 100
+    x = arange(xMin, xMax, step)
     y = a * x**2 + b * x + c
     fig = plt.figure()
     plt.plot(x, y)
@@ -29,12 +30,22 @@ def plot_graphic(a, b, c, delta):
     data = img_data.getvalue()
     return data
 
+def convert_into_number(num):
+    num = num.replace('dividedBy', '/')
+    if num.find('/') == -1:
+        num = float(num)
+        num = int(num) if num % 1 == 0 else rational((num * 10**6), 10**6)
+    else:
+        num = sympify(num)
+
+    return num
+
 
 @app.route('/')
 def roots_vertex():
-    a = int(request.args.get('a'))
-    b = int(request.args.get('b'))
-    c = int(request.args.get('c'))
+    a = convert_into_number(request.args.get('a'))
+    b = convert_into_number(request.args.get('b'))
+    c = convert_into_number(request.args.get('c'))
     delta = b**2 - 4 * a * c
     x = symbols('x')
     y = a * x**2 + b * x + c
@@ -46,21 +57,20 @@ def roots_vertex():
         x2Sym = str(root[1])
         x1Num = float(root[0])
         x2Num = float(root[1])
-        x1NumFormated = f'= {x1Num}' if (x1Num * 10**5) % 1 == 0 else 'approx {:.5f}'.format(x1Num)
-        x2NumFormated = f'= {x2Num}' if (x2Num * 10**5) % 1 == 0 else 'approx {:.5f}'.format(x2Num)
+        x1NumFormated = f'= {x1Num}' if (x1Num * 10**5) % 1 == 0 else 'approx {:.6f}'.format(x1Num)
+        x2NumFormated = f'= {x2Num}' if (x2Num * 10**5) % 1 == 0 else 'approx {:.6f}'.format(x2Num)
         x1 = x1Sym if x1Num % 1 == 0 else f'{x1Sym} {x1NumFormated}'
         x2 = x2Sym if x2Num % 1 == 0 else f'{x2Sym} {x2NumFormated}'
         realRoots = 2
     elif delta == 0:
         xSym = str(root[0])
         xNum = float(root[0])
-        xNumFormated = f'= {xNum}' if (xNum * 10**5) % 1 == 0 else '\u2248 {:.5f}...'.format(xNum)
+        xNumFormated = f'= {xNum}' if (xNum * 10**5) % 1 == 0 else '\u2248 {:.6f}...'.format(xNum)
         x1 = x2 = xSym if xNum % 1 == 0 else f'{xSym} {xNumFormated}'
         realRoots = 1
     else:
         x1 = x2 = ''
         realRoots = 0
-
     xVertex = str(rational(-b, (2 * a)))
     yVertex = str(rational(-delta, (4 * a)))
     response = jsonify(FORM=form,
@@ -69,8 +79,7 @@ def roots_vertex():
                        X1=x1,
                        X2=x2,
                        XVERTEX=xVertex,
-                       YVERTEX=yVertex,
-                       )
+                       YVERTEX=yVertex)
     response.mimetype = 'application/json'
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
--- Web/Src/Script.ts
+++ Web/Src/Script.ts
@@ -1,31 +1,57 @@
 const AINPUT: HTMLInputElement = document.querySelector("#aValue"),
   BINPUT: HTMLInputElement = document.querySelector("#bValue"),
   CINPUT: HTMLInputElement = document.querySelector("#cValue"),
-  FORM: HTMLFormElement = document.querySelector("#input-form");
+  FORM: HTMLFormElement = document.querySelector("#input-form"),
+  FORMDIV: HTMLSpanElement = document.querySelector("#function-form span"),
+  ROOTSDIV: HTMLSpanElement = document.querySelector("#roots span"),
+  RESULTROOTSDIV: HTMLSpanElement = document.querySelector("#result-roots span"),
+  COORDINATESDIV: HTMLSpanElement = document.querySelector("#coordinates span"),
+  RESULTCOORDINATESDIV: HTMLSpanElement = document.querySelector("#result-coordinates span"),
+  GRAPHICDIV: HTMLDivElement = document.querySelector("#graphic");
+
+function testAndGetInput(el: HTMLInputElement) :string {
+  const FractionRegEx = /^-?\d+[/]\d+$/,
+    FloatRegEx = /^-?\d*[.]\d{1,6}$/,
+    IntegerRegEx = /^-?\d+$/,
+    DivideByZeroRegEx = /[/]0/,
+    INPUT = el.value.trim(),
+    VALIDFRACTION = FractionRegEx.test(INPUT) && !DivideByZeroRegEx.test(INPUT),
+    VALIDEFLOAT = FloatRegEx.test(INPUT),
+    VALIDEINTEGER = IntegerRegEx.test(INPUT),
+    ParentEl = el.parentElement;
+  try{
+    if(el.id === "aValue" && (/^-?0+[/]/.test(INPUT) || /^-?0+[.]0+$/.test(INPUT))){
+      throw "Please, enter a non-zero number!";
+    }
+    if(VALIDFRACTION || VALIDEFLOAT || VALIDEINTEGER){
+      return INPUT.replace("/", "dividedBy");
+    }else {
+      throw "Please, enter a integer, float or fractional number!";
+    }
+  }catch (err){
+    const ErrorSpan = document.createElement("span");
+    ErrorSpan.setAttribute("class", "invalid-message")
+    ErrorSpan.textContent = err;
+    ParentEl.setAttribute("class", "invalid-input")
+    ParentEl.appendChild(ErrorSpan)
+    el.addEventListener("focus", () => {
+      ParentEl.removeAttribute("class");
+      ParentEl.lastElementChild.remove();
+    }, {once: true});
+    throw "Invalid Input!"
+  }
+  
+}
 
 //Function that calls the Api
 
 async function calculate() {
-  const A: number = parseInt(AINPUT.value),
-    B: number = parseInt(BINPUT.value),
-    C: number = parseInt(CINPUT.value),
-    ROOTSDIV: HTMLSpanElement = document.querySelector("#roots span"),
-    ROOTSRESULTDIV: HTMLSpanElement =
-      document.querySelector("#result-roots span"),
-    VERTEX: HTMLSpanElement = document.querySelector("#coordinates span"),
-    RESULTCOORDINATES: HTMLSpanElement = document.querySelector(
-      "#result-coordinates span"
-    ),
-    FORMDIV: HTMLSpanElement = document.querySelector("#function-form span"),
-    GRAPHICDIV: HTMLDivElement = document.querySelector("#graphic");
   GRAPHICDIV.innerHTML = null;
   try {
-    if (isNaN(A) || isNaN(B) || isNaN(C)) {
-      throw 'In a quadratic function "a", "b" and "c" must be numbers!';
-    } else if (A === 0) {
-      throw 'In a quadratic function "a" must be a number NOT equal 0!';
-    }
-    const {
+    const A = testAndGetInput(AINPUT),
+      B = testAndGetInput(BINPUT),
+      C = testAndGetInput(CINPUT),
+      {
         X1,
         X2,
         XVERTEX,
@@ -44,7 +70,7 @@ async function calculate() {
       } = await (
         await fetch(`http://127.0.0.1:5000/?a=${A}&b=${B}&c=${C}`)
       ).json(),
-      GRAPHICSVG: HTMLElement = document.createElement("svg");
+      GRAPHICSVG = document.createElement("svg");
     let roots: string;
     switch (REALROOTS) {
       case 2:
@@ -56,21 +82,21 @@ async function calculate() {
       default:
         roots = "This quadratic function don't have any real zero.";
     }
-    FORMDIV.textContent = FORM.replace(/[*][*]2/, "\u00b2").replace(/[*]/g, "");
+    FORMDIV.textContent = FORM.replace("**2", "\u00b2").replace(/[*]/g, "");
     ROOTSDIV.textContent = "Roots (when y = 0):";
-    ROOTSRESULTDIV.innerHTML = roots
+    RESULTROOTSDIV.innerHTML = roots
       .replace(/[*]*sqrt/g, "\u221a")
       .replace(/approx/g, "\u2248");
-    VERTEX.textContent = "Vertex:";
-    RESULTCOORDINATES.textContent = `(${XVERTEX}, ${YVERTEX})`;
+    COORDINATESDIV.textContent = "Vertex:";
+    RESULTCOORDINATESDIV.textContent = `(${XVERTEX}, ${YVERTEX})`;
     GRAPHICSVG.innerHTML = GRAPHIC;
     GRAPHICDIV.appendChild(GRAPHICSVG);
   } catch (err) {
     FORMDIV.textContent = "y = ax\u00b2 + bx + c";
     ROOTSDIV.textContent = null;
-    ROOTSRESULTDIV.textContent = null;
-    VERTEX.textContent = null;
-    RESULTCOORDINATES.textContent = err;
+    RESULTROOTSDIV.textContent = null;
+    COORDINATESDIV.textContent = null;
+    RESULTCOORDINATESDIV.textContent = err.message === "Failed to fetch" ? err.message : err;
   }
   location.href = "#result";
 }
@@ -95,7 +121,7 @@ const defaultValue = function (value: string) {
     }
   };
 
-FORM.onsubmit = function (e: SubmitEvent) {
+FORM.onsubmit = function (e) {
   calculate();
   if (e) {
     e.preventDefault();
@@ -105,6 +131,6 @@ FORM.onsubmit = function (e: SubmitEvent) {
 AINPUT.onblur = defaultValue.bind(AINPUT, "1");
 BINPUT.onblur = defaultValue.bind(BINPUT, "0");
 CINPUT.onblur = defaultValue.bind(CINPUT, "0");
-AINPUT.onkeydown = (e) => whenKeyDown.apply(AINPUT, [BINPUT, e]);
-BINPUT.onkeydown = (e) => whenKeyDown.apply(BINPUT, [CINPUT, e]);
-CINPUT.onkeydown = (e) => whenKeyDown.apply(CINPUT, [FORM, e]);
+AINPUT.onkeydown = (e) => whenKeyDown.call(AINPUT, BINPUT, e);
+BINPUT.onkeydown = (e) => whenKeyDown.call(BINPUT, CINPUT, e);
+CINPUT.onkeydown = (e) => whenKeyDown.call(CINPUT, FORM, e);
