25e05457b0f1e6978ecb98ec831ef0fb05a3ed34
amartinsmg <andremindurimg@gmail.com>
2022-05-08 20:47:16 -0300

    Frontend update

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -15,7 +15,7 @@ class QFCalculator {
   readonly roots: string;
   readonly vertex: string;
   readonly form: string;
-  readonly graph: HTMLElement;
+  readonly graph: string;
 
   private constructor({ roots, vertex, form, graph }: IAPIDataSucess) {
     const [R1, R2] = roots.map((i) =>
@@ -33,75 +33,75 @@ class QFCalculator {
     }
     this.vertex = `(${vertex.join(", ")})`;
     this.form = form.replace("**2", "\u00b2").replace(/[*]/g, "");
-    this.graph = document.createElement("svg");
-    this.graph.innerHTML = graph;
+    this.graph = graph;
   }
 
   public static main(): void {
     //Constatns that store elements that will be often read or changed
 
-    const AInput: HTMLInputElement = document.querySelector("#a-input"),
+    const displayMessage = QFCalculator.displayMessage,
+      getValue = QFCalculator.getValue,
+      testInput = QFCalculator.testInput,
+      whenKeyDown = QFCalculator.whenKeyDown,
+      FormDiv: HTMLDivElement = document.querySelector("#function-form"),
+      AInput: HTMLInputElement = document.querySelector("#a-input"),
       BInput: HTMLInputElement = document.querySelector("#b-input"),
       CInput: HTMLInputElement = document.querySelector("#c-input"),
       Form: HTMLFormElement = document.querySelector("#input-form"),
-      FormDiv: HTMLDivElement = document.querySelector("#function-form"),
-      RootsDiv: HTMLDivElement = document.querySelector("#label-roots"),
+      OutputElement: HTMLOutputElement = document.querySelector("#output"),
+      OutputHeadings: NodeListOf<HTMLHeadingElement> =
+        document.querySelectorAll(".output-heading"),
       ResultRootsDiv: HTMLDivElement = document.querySelector("#roots"),
-      CoordinatesDiv: HTMLDivElement = document.querySelector("#label-coordinates"),
-      ResultCoordinatesDiv: HTMLDivElement = document.querySelector("#coordinates"),
-      GraphDiv: HTMLDivElement = document.querySelector("#graph");
+      ResultCoordinatesDiv: HTMLDivElement =
+        document.querySelector("#coordinates"),
+      GraphFig: HTMLElement = document.querySelector("#graph");
 
     //Call the API, read its response and update the document when form is submitted
 
     Form.onsubmit = async (e) => {
       e.preventDefault();
-      GraphDiv.innerHTML = null;
+      GraphFig.innerHTML = null;
       try {
         try {
-          QFCalculator.testInput([AInput, BInput, CInput]);
+          testInput([AInput, BInput, CInput]);
         } catch {
           throw "Invalid input!";
         }
-        const A = QFCalculator.getValue(AInput),
-          B = QFCalculator.getValue(BInput),
-          C = QFCalculator.getValue(CInput),
+        const A = getValue(AInput),
+          B = getValue(BInput),
+          C = getValue(CInput),
           RESPONSE = await fetch(`http://127.0.0.1:5000/?a=${A}&b=${B}&c=${C}`),
           Data: APIData = await RESPONSE.json();
         if ("error" in Data) {
           throw Data.error;
         }
         const QuadraticFunction = new QFCalculator(Data);
+        OutputHeadings.forEach((el) => el.classList.remove("non-display"));
         FormDiv.textContent = QuadraticFunction.form;
-        RootsDiv.textContent = "Roots (when y = 0):";
         ResultRootsDiv.innerHTML = QuadraticFunction.roots;
-        CoordinatesDiv.textContent = "Vertex:";
         ResultCoordinatesDiv.textContent = QuadraticFunction.vertex;
-        GraphDiv.appendChild(QuadraticFunction.graph);
+        GraphFig.innerHTML = QuadraticFunction.graph;
       } catch (err) {
         FormDiv.textContent = "y = ax\u00b2 + bx + c";
-        RootsDiv.textContent = null;
+        OutputHeadings.forEach((el) => el.classList.add("non-display"));
         ResultRootsDiv.textContent = null;
-        CoordinatesDiv.textContent = null;
         ResultCoordinatesDiv.textContent =
           err instanceof Error ? err.message : err;
       }
-      location.hash = "output";
+      OutputElement.scrollIntoView();
     };
 
     //Methods that call the testInput method for their parent objects when they lose focus
 
-    AInput.onblur = () =>
-      QFCalculator.testInput([AInput], QFCalculator.displayMessage);
-    BInput.onblur = () =>
-      QFCalculator.testInput([BInput], QFCalculator.displayMessage);
-    CInput.onblur = () =>
-      QFCalculator.testInput([CInput], QFCalculator.displayMessage);
+    AInput.onblur = () => testInput([AInput], displayMessage);
+    BInput.onblur = () => testInput([BInput], displayMessage);
+    CInput.onblur = () => testInput([CInput], displayMessage);
 
     //Methods that call the QFCalculator.whenKeyDown function when a key is downed
 
-    AInput.onkeydown = (e) => QFCalculator.whenKeyDown(e, BInput);
-    BInput.onkeydown = (e) => QFCalculator.whenKeyDown(e, CInput);
-    CInput.onkeydown = (e) => QFCalculator.whenKeyDown(e);
+    AInput.onkeydown = (e) => whenKeyDown(e, BInput);
+    BInput.onkeydown = (e) => whenKeyDown(e, CInput);
+    CInput.onkeydown = (e) => whenKeyDown(e);
   }
 
   //Read an element's value and check if it's valid
@@ -128,7 +128,7 @@ class QFCalculator {
           ZeroFloatRegEx.test(VALUE) ||
           ZeroIntegerRegEx.test(VALUE);
       try {
-        if (el.id === "a-input" && ZERONUMBER) {
+        if (el.name === "a" && ZERONUMBER) {
           throw "Enter a non-zero number!";
         }
         if (!(VALIDFRACTION || VALIDEFLOAT || VALIDEINTEGER)) {
@@ -166,7 +166,7 @@ class QFCalculator {
   //Get the value of an element and format it
 
   private static getValue(el: HTMLInputElement): string {
-    return el.value.trim().replace("/", "dividedBy");
+    return el.value.trim().replace("/", "dividedBy").replace(",", ".");
   }
 
   //Move focus to the next element or submit the form if there is none
