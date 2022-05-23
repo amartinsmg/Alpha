0ca4edf726e15dabbccf1a0585a58a0fd1166d36
amartinsmg <andremindurimg@gmail.com>
2022-05-23 17:54:37 -0300

    index.ts update

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -1,7 +1,7 @@
 require("mathjax/es5/tex-svg.js");
 require("./style.css");
 
-declare let MathJax: any;
+declare const MathJax: any;
 
 interface IAPIDataSucess {
   readonly roots: string[];
@@ -23,21 +23,20 @@ class QFCalculator {
   readonly graph: string;
 
   private constructor({ roots, vertex, formula, graph }: IAPIDataSucess) {
-    const convertTexToSvg = QFCalculator.convertTexToSvg;
     switch (roots.length) {
       case 2:
         this.roots = roots
-          .map((e, i) => convertTexToSvg(`{x}_{${i + 1}} = ${e}`))
+          .map((e, i) => QFCalculator.convertTexToSvg(`{x}_{${i + 1}} = ${e}`))
           .join("");
         break;
       case 1:
-        this.roots = convertTexToSvg(`x = ${roots.pop()}`);
+        this.roots = QFCalculator.convertTexToSvg(`x = ${roots.pop()}`);
         break;
       default:
         this.roots = "This quadratic function don't have any real zero.";
     }
-    this.vertex = convertTexToSvg(`(${vertex.join(", ")})`);
-    this.formula = convertTexToSvg(formula);
+    this.vertex = QFCalculator.convertTexToSvg(`(${vertex.join(", ")})`);
+    this.formula = QFCalculator.convertTexToSvg(formula);
     this.graph = graph;
   }
 
@@ -57,7 +56,7 @@ class QFCalculator {
 
   private static testInputGetValue(
     els: HTMLInputElement[],
-    catchEroor?: Function,
+    catchError?: Function,
     parentForm?: HTMLFormElement
   ): string[] {
     const VALUES = els.map((el) => {
@@ -69,20 +68,19 @@ class QFCalculator {
         ZeroFractionRegEx = /^-?0+dividedBy/,
         ZeroFloatRegEx = /^-?0*[.]0+$/,
         ZeroIntegerRegEx = /^-?0+$/,
-        VALIDFRACTION =
-          FractionRegEx.test(VALUE) && !DividedByZeroRegEx.test(VALUE),
-        VALIDEFLOAT = FloatRegEx.test(VALUE),
-        VALIDEINTEGER = IntegerRegEx.test(VALUE),
-        VALIDENUMBER = VALIDFRACTION || VALIDEFLOAT || VALIDEINTEGER,
-        ZERONUMBER =
+        VALID_NUMBER =
+          (FractionRegEx.test(VALUE) && !DividedByZeroRegEx.test(VALUE)) ||
+          FloatRegEx.test(VALUE) ||
+          IntegerRegEx.test(VALUE),
+        ZERO_NUMBER =
           ZeroFractionRegEx.test(VALUE) ||
           ZeroFloatRegEx.test(VALUE) ||
           ZeroIntegerRegEx.test(VALUE);
       try {
-        if (!VALIDENUMBER) throw "Enter a integer, float or fractional number!";
-        if (el.name === "a" && ZERONUMBER) throw "Enter a non-zero number!";
+        if (!VALID_NUMBER) throw "Enter a integer, float or fractional number!";
+        if (el.name === "a" && ZERO_NUMBER) throw "Enter a non-zero number!";
       } catch (err) {
-        if (catchEroor) catchEroor(el, err, parentForm);
+        if (catchError) catchError(el, err, parentForm);
         else throw "Invalid input!";
       }
       return VALUE;
@@ -134,13 +132,11 @@ class QFCalculator {
   public static main(): void {
     //Constatns that store elements that will be often read or changed
 
-    const showFeedback = QFCalculator.showFeedback,
-      testInputGetValue = QFCalculator.testInputGetValue,
-      whenKeyDown = QFCalculator.whenKeyDown,
-      FormulaDiv: HTMLDivElement = document.querySelector("#formula"),
+    const FormulaDiv: HTMLDivElement = document.querySelector("#formula"),
       AInput: HTMLInputElement = document.querySelector("#a-input"),
       BInput: HTMLInputElement = document.querySelector("#b-input"),
       CInput: HTMLInputElement = document.querySelector("#c-input"),
+      InputsElements = [AInput, BInput, CInput],
       Form: HTMLFormElement = document.querySelector("#coeficients-form"),
       OutputElement: HTMLOutputElement = document.querySelector("#output-data"),
       OutputHeadings: NodeListOf<HTMLHeadingElement> =
@@ -157,11 +153,11 @@ class QFCalculator {
       e.preventDefault();
       GraphFig.innerHTML = null;
       try {
-        const [A, B, C] = testInputGetValue([AInput, BInput, CInput]),
-          RESPONSE = await fetch(
+        const [A, B, C] = QFCalculator.testInputGetValue(InputsElements),
+          FetchResponse = await fetch(
             `http://${location.hostname}:5000/?a=${A}&b=${B}&c=${C}`
           ),
-          Data: APIData = await RESPONSE.json();
+          Data: APIData = await FetchResponse.json();
         if ("error" in Data) throw Data.error;
         const QuadraticFunction = new QFCalculator(Data);
         OutputHeadings.forEach((el) => el.classList.remove("non-display"));
@@ -170,7 +166,8 @@ class QFCalculator {
         CoordinatesDiv.innerHTML = QuadraticFunction.vertex;
         GraphFig.innerHTML = QuadraticFunction.graph;
       } catch (err) {
-        FormulaDiv.innerHTML = QFCalculator.convertTexToSvg("y = ax^2 + bx + c");
+        FormulaDiv.innerHTML =
+          QFCalculator.convertTexToSvg("y = ax^2 + bx + c");
         OutputHeadings.forEach((el) => el.classList.add("non-display"));
         RootsDiv.innerHTML = null;
         CoordinatesDiv.innerHTML = null;
@@ -183,17 +180,20 @@ class QFCalculator {
       OutputElement.scrollIntoView();
     };
 
-    //Methods that call the testInputGetValue method for their parent objects when they lose focus
+    for (let [i, el] of InputsElements.entries()) {
+      //Method that calls the testInputGetValue method for their parent objects when its lose focus
 
-    AInput.onblur = () => void testInputGetValue([AInput], showFeedback, Form);
-    BInput.onblur = () => void testInputGetValue([BInput], showFeedback, Form);
-    CInput.onblur = () => void testInputGetValue([CInput], showFeedback, Form);
+      el.onblur = () =>
+        void QFCalculator.testInputGetValue(
+          [el],
+          QFCalculator.showFeedback,
+          Form
+        );
 
-    //Methods that call the QFCalculator.whenKeyDown function when a key is downed
+      //Method that call the whenKeyDown method when a key is downed
 
-    AInput.onkeydown = (e) => whenKeyDown(e, BInput);
-    BInput.onkeydown = (e) => whenKeyDown(e, CInput);
-    CInput.onkeydown = (e) => whenKeyDown(e);
+      el.onkeydown = (e) => QFCalculator.whenKeyDown(e, InputsElements[i + 1]);
+    }
   }
 }
 
