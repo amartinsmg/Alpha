ccc6870efdf3826b9b9c0b3ede8732d4a852c85b
amartinsmg <andremindurimg@gmail.com>
2022-06-02 20:08:07 -0300

    plotpoits.ts update

--- src/ts/plotpoints.ts
+++ src/ts/plotpoints.ts
@@ -7,24 +7,17 @@ class PlotPoits {
   //Take the formula and the roots or x-coordinate to get the graph points
 
   public constructor(formula: string, ...xValues: number[]) {
-    const Expression = compile(formula),
+    const Expression: any = compile(formula),
       FilteredXValues = xValues
         .filter((x) => typeof x === "number" && !isNaN(x))
         .sort(),
       X_1 = FilteredXValues[0] ?? 0,
-      X_2 = FilteredXValues[FilteredXValues.length - 1] ?? null;
-    let xMinValue: number, xMaxValue: number;
-    if (X_2 === 0 || X_2) {
-      const EXTRA_VALUE = max(2, abs(X_2 - X_1) / 5, min(abs(X_1), abs(X_2)));
-      xMinValue = X_1 - EXTRA_VALUE;
-      xMaxValue = X_2 + EXTRA_VALUE;
-    } else {
-      const EXTRA_VALUE = max(2, abs(X_1));
-      xMinValue = X_1 - EXTRA_VALUE;
-      xMaxValue = X_1 + EXTRA_VALUE;
-    }
-    const STEP = abs(xMaxValue - xMinValue) / 100,
-      x: number[] = range(xMinValue, xMaxValue, STEP).toArray(),
+      X_2 = FilteredXValues[FilteredXValues.length - 1] ?? X_1,
+      EXTRA_VALUE: number = max(2, abs(X_2 - X_1) / 5, min(abs(X_1), abs(X_2))),
+      X_MIN_VALUE = X_1 - EXTRA_VALUE,
+      X_MAX_VALUE = X_2 + EXTRA_VALUE,
+      STEP = abs(X_MAX_VALUE - X_MIN_VALUE) / 100,
+      x: number[] = range(X_MIN_VALUE, X_MAX_VALUE, STEP).toArray(),
       y: number[] = x.map((x) => Expression.evaluate({ x }));
     return { x, y };
   }
