1ce72e6b98d7526111b2772b2d9343758a8a5f5b
amartinsmg <andremindurimg@gmail.com>
2022-07-14 20:32:23 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import "./css/main.css";
 import QuadraticFunctionCalculator from "./ts/qfcalculator";
 
-window.onload = () =>
-  void new QuadraticFunctionCalculator(
-    "#formula",
-    ["#a-input", "#b-input", "#c-input"],
-    "#coefficients-form",
-    "#output-data",
-    ".output-heading",
-    "#roots",
-    "#coordinates",
-    "#graph",
-    "#error-feedback"
-  );
+window.addEventListener(
+  "load",
+  () =>
+    void new QuadraticFunctionCalculator(
+      "#formula",
+      ["#a-input", "#b-input", "#c-input"],
+      "#coefficients-form",
+      "#output-data",
+      ".output-heading",
+      "#roots",
+      "#coordinates",
+      "#graph",
+      "#error-feedback"
+    )
+);
