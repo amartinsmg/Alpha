a5af95c5c04e05c0b35c9b375848abab19f22430
andre <andremindurimg@gmail.com>
2023-04-21 19:45:56 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import "./css/main.css";
 import QuadraticFunctionCalculator from "./ts/qfcalculator";
 
-/**
-  instantiates the QuadraticFunctionCalculator class when the web page is fully loaded.
-  It ensures that all necessary resources and elements are available before the
-  calculator is initialized, preventing errors that could occur if the calculator were
-  initialized too early
- */
+// Instantiates the QuadraticFunctionCalculator class when the web page is fully loaded.
 
 window.addEventListener(
   "load",
